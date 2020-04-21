43aaef166904b81f5d9dcea935cdc1ec4ed7b400
lostmap <lostmap4@gmail.com>
2020-04-21 04:52:28 +0300

    summernote model field + we need container

--- openstack_dashboard/dashboards/news/overview/models.py
+++ openstack_dashboard/dashboards/news/overview/models.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+from django_summernote.fields import SummernoteTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = SummernoteTextField()
     created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
