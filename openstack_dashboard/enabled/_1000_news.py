361a8aa9a15230f8a6dfe2ebf0889b16c4f81f30
lostmap <lostmap4@gmail.com>
2020-06-01 13:01:33 +0300

    add enabled with news

---
+++ openstack_dashboard/enabled/_1000_news.py
@@ -0,0 +1,13 @@
+
+# The name of the dashboard to be added to HORIZON['dashboards']. Required.
+DASHBOARD = 'news'
+# If set to True, this dashboard will be set as the default dashboard.
+DEFAULT = True
+# If set to True, this dashboard will not be added to the settings.
+DISABLED = False
+
+# A list of applications to be added to INSTALLED_APPS.
+ADD_INSTALLED_APPS = [
+    'openstack_dashboard.dashboards.news',
+    'django_summernote'
+]
